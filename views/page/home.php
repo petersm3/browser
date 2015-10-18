13311c70f59d0abf5d4630aa33cc199175a72738
9d08737aa35cf7f15939758ab3331b9c <petersm3@onid.oregonstate.edu>
2015-10-18 09:39:58 -0700

    Implemented basic form with onsubmit with checkboxes to post results

--- views/page/home.php
+++ views/page/home.php
@@ -11,16 +11,18 @@ $this->setLayoutVar('pageTitle', 'Image and cultural properties browser');
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Browser <span class="glyphicon glyphicon-refresh" aria-hidden="true"></span></a>
+      <a class="navbar-brand" href="/">Browser <span class="glyphicon glyphicon-refresh" aria-hidden="true"></span></a>
     </div>
-
+<form method="post" action="/testo">
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Creator<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li>&nbsp;<input type="checkbox" aria-label="Lawrence,_Ellis_Fuller">&nbsp;Lawrence, Ellis Fuller</li>
+            <li>&nbsp;<input type="checkbox" aria-label="Lawrence,_Ellis_Fuller" onchange="this.form.submit();">&nbsp;Lawrence, Ellis Fuller</li>
+            <li>&nbsp;<input type="checkbox" aria-label="Holford,_William" onchange="this.form.submit();">&nbsp;Holford, William</li>
+            <li>&nbsp;<input type="checkbox" aria-label="Doyle,_Albert_E." onchange="this.form.submit();">&nbsp;Doyle, Albert E.</li>
           </ul>
         </li>
       </ul>
@@ -29,12 +31,13 @@ $this->setLayoutVar('pageTitle', 'Image and cultural properties browser');
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expand
 ed="false">Photographer<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
+            <li>&nbsp;<input type="checkbox" aria-label="Ross,_Marion_Dean" onchange="this.form.submit();">&nbsp;Ross, Marion Dean</li>
+            <li>&nbsp;<input type="checkbox" aria-label="Shellenbarger,_Michael" onchange="this.form.submit();">&nbsp;Shellenbarger, Michael</li>
+            <li>&nbsp;<input type="checkbox" aria-label="Teague,_Edward,_1952-" onchange="this.form.submit();">&nbsp;Teague, Edward, 1952-</li>
           </ul>
         </li>
       </ul>
+</form>
       <ul class="nav navbar-nav">
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expand
