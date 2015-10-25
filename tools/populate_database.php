53f5076ae8b0cdd7e74e93b4848f64950f3623f4
9d08737aa35cf7f15939758ab3331b9c <petersm3@onid.oregonstate.edu>
2015-10-25 00:17:50 -0700

    Correctly renaming files

--- tools/populate_database.php
+++ tools/populate_database.php
@@ -1,8 +1,12 @@
 #!/usr/bin/php
 <?php
-
-require_once('../config/config.php');
 require_once('../classes/Database.php');
+// browser_www user only has SELECT permissions in ../config/config.php
+// Specify a user with INSERT credentials in credentials.php
+require_once('./credentials.php'):
+
+/* Not using PDO transactions with exception handling in this script.
+ */
 
 $database = new Database;
 $dbh = $database->getConnection(); // Get database handle
@@ -12,8 +16,17 @@ if(!isset($argv[1])) {
     echo ' e.g., ' . $argv[0] . ' 10000' . "\n";
     echo "\n";
     echo "This script will populate the following tables with sample data:\n";
-    echo "`properties`, `categories`, `filters`, and `attributes`\n\n";
+    echo "`categories`, `properties`, `filters`, and `attributes`\n\n";
     exit();
 };
+
+// Populate `categories`
+// INSERT INTO categories (priority, category, subcategory) VALUES (1, 'Creator', 'Lawrence, Ellis Fuller');
+$sql= "INSERT INTO categories (priority, category, subcategory)";
+$sql.= " VALUES (?, ?, ?)";
+$values = array(1, 'Creator', 'Lawrence, Ellis Fuller');
+$st=$dbh->prepare($sql);
+$st->execute($values);
+
 
 ?>
